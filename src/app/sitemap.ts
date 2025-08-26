5027405cc3656e6e13527adbf1f6b0db7e8e7e06
TheLingeringWill <jonathan.el.baz@outlook.com>
2025-08-26 13:00:20 +0200

    back to next_public url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
+  const baseUrl = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000'
   
   return [
     {
